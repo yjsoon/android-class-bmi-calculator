9572fbc2cd70b085697862a879261b2def13616c
YJ Soon <yjsoon@tinkertanker.com>
2015-05-27 15:16:36 +0800

    Filled in starter code for Step 3

--- app/src/main/java/com/tinkercademy/bmicalculator/MainActivity.java
+++ app/src/main/java/com/tinkercademy/bmicalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.tinkercademy.bmicalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,15 +16,25 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText weightField = (EditText) findViewById(R.id.weight_field);
-        EditText heightField = (EditText) findViewById(R.id.height_field);
+        final EditText weightField = (EditText) findViewById(R.id.weight_field);
+        final EditText heightField = (EditText) findViewById(R.id.height_field);
         TextView resultsText = (TextView) findViewById(R.id.results_text);
         Button calculateButton = (Button) findViewById(R.id.calculate_button);
 
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // more code goes here later
+
+                String weightString = weightField.getText().toString();
+                String heightString = heightField.getText().toString();
+
+                double weight = Double.parseDouble(weightString);
+                double height = Double.parseDouble(heightString);
+
+                Log.d("BMICALC", "Weight is " + weight + ", height is " + height);
+
+                // Can you fill in the rest, and set resultsText to show the result?
+
             }
         });
 
