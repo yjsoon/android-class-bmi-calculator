e40c8752532de5eaf988cb7e0ff7a355c9a0d023
YJ Soon <yjsoon@tinkertanker.com>
2015-05-27 14:53:04 +0800

    Finished Step 2: find Views by ID

--- app/src/main/java/com/tinkercademy/bmicalculator/MainActivity.java
+++ app/src/main/java/com/tinkercademy/bmicalculator/MainActivity.java
@@ -1,9 +1,11 @@
 package com.tinkercademy.bmicalculator;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,19 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText weightField = (EditText) findViewById(R.id.weight_field);
+        EditText heightField = (EditText) findViewById(R.id.height_field);
+        TextView resultsText = (TextView) findViewById(R.id.results_text);
+        Button calculateButton = (Button) findViewById(R.id.calculate_button);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // more code goes here later
+            }
+        });
+
     }
 
 }
